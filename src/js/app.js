e131066f2e404bff6566d2f4e238046f12afe37c
Riks23 <125542543+Riks23@users.noreply.github.com>
2023-03-09 16:07:57 +0300

    Delete src directory

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('#nav li').hover(function(){
-        $(this).children('ul').stop(false, true).fadeIn(500);
-    }, function(){
-        $(this).children('ul').stop(false, true).fadeOut(500);
-    });
-});
